90a291d784c3ff00de3465ae1969c008bc14e44e
Mikhail Syniuk <msyniuk@gmail.com>
2017-08-19 17:41:22 +0300

    Get class

--- app.php
+++ app.php
@@ -1,15 +1,8 @@
 <?php
-function getHtml($url)
-{
 
-}
+require_once ('get.class.php');
 
-function getBlock($regex)
-{
-
-}
-
-function getTemperatureArray()
-{
-
-}
+$html = getHtml('http://rp5.ua/Weather_in_Dnipro_(Dnipropetrovsk)');
+//getBlock('/id\=\"forecastTable".+?\/table/s', $html);
+$block = getBlock('/id\=\"t_temperature".+?\/tr/s', $html);
+print_r(getTemperatureArray($block[0]));
