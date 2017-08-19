f6aee49244e5af0bbed0a8515c5b2b43075e66cf
Lithium1987 <vladkrents@gmail.com>
2017-08-19 14:15:39 +0300

    add temp

--- app.php
+++ app.php
@@ -1,12 +1,41 @@
 <?php
 function getHtml($url){
+$html = file_get_contents($url);
+return $html;
+}
+
+function getBlock($regex, $subject){
+
+    $array = [];
+    preg_match_all($regex, $subject, $array);
+    return $array;
 
 }
 
-function getBlock($regexp){
+function getTemperatureArray($temp){
+    $array = [];
+    $regex = '/class\=\"t_0\"*class\=\"otstup\".+?\/tr/s';
+    preg_match_all()
+
 
 }
 
-function getTempperatureArray(){
-    
-}
+//print getHtml('http://rp5.ua/Weather_in_Dnipro_(Dnipropetrovsk)');
+
+$html = getHtml('http://rp5.ua/Weather_in_Dnipro_(Dnipropetrovsk)');
+//print_r(getBlock(
+//    '/id\=\"forecastTable\".+?\/table/s',
+//    $html)
+//);
+$temp = getBlock(
+    '/id\=\"forecastTable\".+?\/table/s',
+    $html);
+print_r(
+    getTemperatureArray($temp[0])
+);
+
+
+
+
+
+?>
