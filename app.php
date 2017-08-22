aa147157e9ef382b3e90f9c8719894557a263cb3
Lea <dn290188sai@gmail.com>
2017-08-22 17:38:42 +0300

    app.php get.class.php

---
+++ app.php
@@ -0,0 +1,6 @@
+<?php
+require_once ('get.class.php');
+$html = getHtml('http://rp5.ua/Weather_in_Dnipro_(Dnipropetrovsk)');
+//getBlock('/id\=\"forecastTable".+?\/table/s', $html);
+$block = getBlock('/id\=\"t_temperature".+?\/tr/s', $html);
+print_r(getTemperatureArray($block[0]));
